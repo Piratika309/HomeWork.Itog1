046afa7be85a15bd4243ad296fc79bd834a1693d
Natali <piratika309@mail.ru>
2023-03-15 02:12:02 +0300

    Код программы, решающий задачу

---
+++ example/Program.cs
@@ -0,0 +1,26 @@
+﻿string[] array = {"терпение", "и", "труд", "всё", "перетрут"};
+string[] result = new string[array.Length];
+
+void FillArray(string[] array, string[] result)
+{
+    int count = 0;
+    for (int i = 0; i<array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            result[count] = array[i];
+            count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i=0; i<array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+FillArray(array, result);
+PrintArray (result);
